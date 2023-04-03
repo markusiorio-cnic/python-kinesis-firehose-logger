413c5bc1b0f0c2e6af1f97e938d4e902b3a2b606
Markus Iorio <112956343+markusiorio-cnic@users.noreply.github.com>
2023-04-03 12:53:36 +0200

    Update kinesishandler.py

--- kinesishandler/kinesishandler.py
+++ kinesishandler/kinesishandler.py
@@ -1,40 +1,42 @@
-#
-# Copyright (C) 2016 Tomas Nilsson (joekickass). All rights reserved.
-#
+import logging 
+import logging.handlers as handler
+import typing
+
+if typing.TYPE_CHECKING:
+    from queue import Queue
 
 
-from logging.handlers import BufferingHandler
-
-
-class KinesisHandler(BufferingHandler):
+class KinesisHandler:
     """
     Sends logs in batches to Kinesis Firehose
 
     Uses a queue to dispatch batched data to worker thread
     """
 
-    def __init__(self, capacity, queue) -> None:
+    def __init__(
+        self, capacity: int, queue: Queue, buffer: handler.BufferingHandler | None = None
+    ) -> None:
         """
         Initialize the handler with buffer size and queue
         """
-        BufferingHandler.__init__(self, capacity)
+        self.buffer = buffer or handler.BufferingHandler(capacity)
         self.queue = queue
 
-    def prepare(self, records) -> list[str]:
+    def prepare(self, records: list[logging.LogRecord]) -> list[str]:
         """
         Prepare data for queuing
 
         TODO: Is self.format() keeping all info? what about errors?
         """
-        return [self.format(record) for record in records]
+        return [self.buffer.format(record) for record in records]
 
     def flush(self) -> None:
         """
         Put buffered data in queue and zap buffer.
         """
-        self.acquire()
+        self.buffer.acquire()
         try:
-            self.queue.put(self.prepare(self.buffer))
-            self.buffer = []
+            self.queue.put(self.prepare(self.buffer.buffer))
+            self.buffer.buffer = []
         finally:
-            self.release()
+            self.buffer.release()
