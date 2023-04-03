79e1a5a816fbe86ad2fa502513d318dcaa8b0062
Markus Iorio <112956343+markusiorio-cnic@users.noreply.github.com>
2023-04-03 11:57:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='kinesishandler',
       version='0.1',
       description='The KinesisHandler is a BufferingHandler that sends logging output to a AWS Kinesis stream',
-      url='https://github.com/joekickass/python-kinesis-logger',
-      author='joekickass',
-      author_email='tnilsson.x@gmail.com',
+      url='https://github.com/markusiorio-cnic/python-kinesis-logger',
+      author='markusiorio',
+      author_email='markus.iorio@centralnic.com',
       license='MIT',
       packages=['kinesishandler'],
       zip_safe=False)
